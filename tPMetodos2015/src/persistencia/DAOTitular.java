517baaa59314f6a2cbda4bcf6ed32c7df2ba1760
matiassequeira <matiassequeirq@gmail.com>
2015-10-26 14:53:42 -0300

    DAOTitular completado

--- tPMetodos2015/src/persistencia/DAOTitular.java
+++ tPMetodos2015/src/persistencia/DAOTitular.java
@@ -1,6 +1,10 @@
 package persistencia;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import clasesDeTablas.Titular;
 
@@ -9,13 +13,21 @@ public class DAOTitular implements Repositorio<Titular> {
 	@Override
 	public void save(Titular objeto) {
 		// TODO Auto-generated method stub
-		
+		 SessionFactory factory= FabricaSessionFactory.getFactory();
+         Session session = factory.getCurrentSession(); 
+         session.beginTransaction();
+         session.save(objeto);
+         session.getTransaction().commit();
 	}
 
 	@Override
 	public void delete(Titular objeto) {
 		// TODO Auto-generated method stub
-		
+		SessionFactory factory= FabricaSessionFactory.getFactory();
+        Session session = factory.getCurrentSession(); 
+        session.beginTransaction();
+        session.delete(objeto);
+        session.getTransaction().commit();
 	}
 
 	@Override
@@ -23,23 +35,53 @@ public class DAOTitular implements Repositorio<Titular> {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public Titular getByDocumento(String documento) {
+		// TODO Auto-generated method stub
+		SessionFactory factory= FabricaSessionFactory.getFactory();
+        Session session = factory.getCurrentSession(); 
+        session.beginTransaction();
+        Titular objeto = (Titular) session.get(Titular.class, documento);
+        session.getTransaction().commit();
+        return objeto;
+	}
 
 	@Override
 	public List<Titular> getAll() {
 		// TODO Auto-generated method stub
-		return null;
+		List<Titular> listF = new ArrayList<Titular>();
+        SessionFactory factory= FabricaSessionFactory.getFactory();
+        Session session = factory.getCurrentSession(); 
+        session.beginTransaction();
+        @SuppressWarnings("unchecked")
+        List<Titular> list = session.createCriteria(Titular.class).list();
+        session.getTransaction().commit();
+        for(Titular o : list) {
+            listF.add((Titular)o);
+        }
+        return listF;
 	}
 
 	@Override
 	public void saveAll(List<Titular> lista) {
 		// TODO Auto-generated method stub
-		
+		SessionFactory factory= FabricaSessionFactory.getFactory();
+        Session session = factory.getCurrentSession(); 
+        session.beginTransaction();
+        for (Titular loc : lista) {
+        session.save(loc);
+        }               
+        session.getTransaction().commit();
 	}
 
 	@Override
 	public void update(Titular objeto) {
 		// TODO Auto-generated method stub
-		
+		 SessionFactory factory= FabricaSessionFactory.getFactory();
+         Session session = factory.getCurrentSession(); 
+         session.beginTransaction();
+         session.update(objeto);
+         session.getTransaction().commit();
 	}
 
 }
