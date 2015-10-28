e23ed8ea1d5f44378ebd39ce52988261ea7c0f78
Rociogonzalez <rp.gonzalez72@gmail.com>
2015-10-28 10:14:52 -0300

    Prueba

--- tPMetodos2015/src/validacion/Validar.java
+++ tPMetodos2015/src/validacion/Validar.java
@@ -4,9 +4,11 @@ import clasesDeTablas.Titular;
 public class Validar {
 	boolean validar(Titular titular){
 		int comprobar=0;
-		titular.getId().getTipoDoc()
+		titular.getId().getTipoDoc();
 		if(titular.getId().getNroDoc().length()==8){
 			comprobar++;
 		}
+		if (comprobar ==11) return true;
+		else return false;
 	}
 }
