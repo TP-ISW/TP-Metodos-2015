06cbb9f534a1c502c1875f0c0b6b535c28b6c6bc
Rociogonzalez <rp.gonzalez72@gmail.com>
2015-10-28 10:10:06 -0300

    Paquete Validar y Excepcion

---
+++ tPMetodos2015/src/excepciones/ExcepcionValidador.java
@@ -0,0 +1,12 @@
+package excepciones;
+
+public class ExcepcionValidador extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExcepcionValidador(String msg){
+		super(msg);
+	}
+}
---
+++ tPMetodos2015/src/validacion/Validar.java
@@ -0,0 +1,12 @@
+package validacion;
+import clasesDeTablas.Titular;
+
+public class Validar {
+	boolean validar(Titular titular){
+		int comprobar=0;
+		titular.getId().getTipoDoc()
+		if(titular.getId().getNroDoc().length()==8){
+			comprobar++;
+		}
+	}
+}
