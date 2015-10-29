adeccb6241ae47330302e7ccdaf3dfe8ccf86cf2
guillejacob <guillejacob1@gmail.com>
2015-10-29 12:31:54 -0300

    se creo el package de prueba y la clase TestAntiguedadLicencia

---
+++ tPMetodos2015/src/prueba/TestAntiguedadLicencia.java
@@ -0,0 +1,17 @@
+package prueba;
+
+import junit.framework.TestCase;
+
+public class TestAntiguedadLicencia extends TestCase {
+
+	  protected String categoriaNuevo, categoriaRenovacion;
+	   
+	   // assigning the values
+	   protected void setUp(){
+	      categoriaNuevo = "nuevo";
+	      categoriaRenovacion = "renovacion";
+	   }
+	   
+	   
+	
+}
