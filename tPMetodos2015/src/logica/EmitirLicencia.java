82d9991e6288ac9adf3cac8f910953da7b6a991a
Guille <Guille@192.168.1.2>
2015-11-03 11:21:10 -0300

    Arreglo de los calendar.

--- tPMetodos2015/src/logica/EmitirLicencia.java
+++ tPMetodos2015/src/logica/EmitirLicencia.java
@@ -14,9 +14,9 @@ public class EmitirLicencia {
 	public static int calcularEdad(Calendar fechaNacimientoTitular){
 		Calendar fechaActual = Calendar.getInstance();
 		
-		int edad = (fechaActual.YEAR) - (fechaNacimientoTitular.YEAR);
+		int edad = (fechaActual.get(Calendar.YEAR)) - (fechaNacimientoTitular.get(Calendar.YEAR));
 		
-		if (fechaNacimientoTitular.DAY_OF_YEAR > fechaActual.DAY_OF_YEAR) //quiere decir que no todavía no cumplió los anios
+		if (fechaNacimientoTitular.get(Calendar.DAY_OF_YEAR) > fechaActual.get(Calendar.DAY_OF_YEAR)) //quiere decir que no todavía no cumplió los anios
 			edad -= 1;
 		
 		return edad;
@@ -28,19 +28,19 @@ public class EmitirLicencia {
 		Calendar fechaNacimientoTitular = titular.getFechaNacimiento();
 		
 		Calendar fechaExpiracion = Calendar.getInstance();
-		fechaExpiracion.set(fechaExpiracion.DAY_OF_MONTH, fechaNacimientoTitular.DAY_OF_MONTH);
-		fechaExpiracion.set(fechaExpiracion.MONTH, fechaNacimientoTitular.MONTH);
+		fechaExpiracion.set(Calendar.DAY_OF_MONTH, fechaNacimientoTitular.get(Calendar.DAY_OF_MONTH));
+		fechaExpiracion.set(Calendar.MONTH, fechaNacimientoTitular.get(Calendar.MONTH));
 
 		int edad = calcularEdad(fechaNacimientoTitular);
 		
 		if((edad < 21 && categoria.equals("nuevo")) || edad > 70)
-			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.YEAR+1);		
+			fechaExpiracion.set(Calendar.YEAR, fechaNacimientoTitular.get(Calendar.YEAR)+1);		
 		if((edad < 21 && categoria.equals("renovacion")) || (edad > 60 && edad <= 70) )
-			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.YEAR+3);
+			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.get(Calendar.YEAR)+3);
 		if(edad >= 21 && edad <= 46)
-			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.YEAR+5);
+			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.get(Calendar.YEAR)+5);
 		if(edad > 46 && edad <= 60)
-			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.YEAR+4);
+			fechaExpiracion.set(fechaExpiracion.YEAR, fechaNacimientoTitular.get(Calendar.YEAR)+4);
 		
 		List<Calendar> fechasVigencia = new ArrayList<Calendar>();
 		fechasVigencia.add(fechaInicioVigencia);
@@ -53,7 +53,7 @@ public class EmitirLicencia {
 		// TODO Auto-generated method stub
 		Calendar fechaActual= Calendar.getInstance();
 		int antiguedad=licVig.getFechaEmision().YEAR - fechaActual.YEAR ;
-		if(licVig.getFechaEmision().DAY_OF_YEAR - fechaActual.DAY_OF_YEAR > 0)
+		if(licVig.getFechaEmision().get(Calendar.DAY_OF_YEAR) - fechaActual.get(Calendar.DAY_OF_YEAR) > 0)
 			antiguedad--;
 		return antiguedad;
 	}
