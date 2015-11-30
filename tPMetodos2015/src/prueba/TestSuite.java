e8c06aea252e7145edb9188b0a2fb0c5551d82fc
guillejacob <guillejacob1@gmail.com>
2015-11-30 10:37:41 -0300

    volviendo a commit anterior?

--- tPMetodos2015/src/prueba/TestSuite.java
+++ tPMetodos2015/src/prueba/TestSuite.java
@@ -5,8 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestAntiguedadLicencia.class,TestDAOTitular.class })
+@SuiteClasses({TestAntiguedadLicencia.class, TestDAOTitular.class})
 public class TestSuite {
 
 }
-
