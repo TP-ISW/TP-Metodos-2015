b8e6fda797b94d3db1188e157c9fc2e5fea267ad
guillejacob <guillejacob1@gmail.com>
2015-11-30 10:44:27 -0300

    Revert "volviendo a commit anterior?"
    This reverts commit e8c06aea252e7145edb9188b0a2fb0c5551d82fc.

--- tPMetodos2015/src/prueba/TestSuite.java
+++ tPMetodos2015/src/prueba/TestSuite.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestAntiguedadLicencia.class, TestDAOTitular.class})
+@SuiteClasses({ TestAntiguedadLicencia.class,TestDAOTitular.class })
 public class TestSuite {
 
 }
+
