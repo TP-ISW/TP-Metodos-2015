bc48a2330547f0eb2b180464a909e6add87c5973
Rociogonzalez <rp.gonzalez72@gmail.com>
2015-12-01 09:43:18 -0300

    Modificacion ExcepcionNull

--- tPMetodos2015/src/excepciones/ExcepcionNull.java
+++ tPMetodos2015/src/excepciones/ExcepcionNull.java
@@ -2,7 +2,11 @@ package excepciones;
 
 public class ExcepcionNull extends Exception {
 	private static final long serialVersionUID = 1L;
-
+	private String mensaje;
 	public ExcepcionNull(String msg){
+		mensaje=msg;
+	}
+	public String getMensaje(){
+		return mensaje;
 	}
 }
